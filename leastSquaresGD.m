f5505a1bcb06ae1a77267792d4b1a13687f9ae7b
Alexander Vostriakov <alexander.vostriakov@epfl.ch>
2015-10-31 16:17:44 +0100

    Gradient

--- leastSquaresGD.m
+++ leastSquaresGD.m
@@ -1,3 +1,21 @@
 function beta = leastSquaresGD(y,tX,alpha)
+    [N,~] = size(y);
+    beta = zeros(N+1,1);
+    maxIters = 1000;
+    lastBeta = beta;
+    convergence= 0.00001;
 
+    for k = 1:maxIters
+    	g = computeGradient(y, tX,beta);
+        beta = beta - alpha .* g;
+        if abs(lastBeta - beta)< beta*convergence
+            break
+        end
+    end
+end
+
+function g = computeGradient(y,tX,beta)
+    [N,~] = size(y);
+    e = y - tX*beta;    %compute error
+    g = tX'*e/(-1*N);   %compute MSE
 end
