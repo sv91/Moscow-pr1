0e7d0bd9fad3494b1e358b3cbe6007312688d267
Mickaël Misbach <mickael@misba.ch>
2015-11-03 15:28:22 +0100

    fixed logisticRegression.m

--- logisticRegression.m
+++ logisticRegression.m
@@ -1,39 +1,66 @@
 function beta = logisticRegression(y,tX,alpha)
-% Initial values
-    [~,N] = size(tX);
-    beta = zeros(N,1);
+    % algorithm parametes
     maxIters = 1000;
-    lastBeta = beta;
-    convergence= 0.00001;
 
-    for k = 1:maxIters
-        beta
-    	g = computeGradient(y, tX,beta);
-        H = computeHessian(tX, beta);
-        beta = beta -  alpha .* (H \ g);
-        if abs(lastBeta - beta)<= beta*convergence 
-            break % If the difference between two step is too small, we stop
-        end
-        lastBeta = beta;
+    % initialize
+    [~,D] = size(tX)
+    beta = zeros(D, 1)
+
+    % iterate
+    fprintf('Starting iterations, press Ctrl+c to break\n');
+    fprintf('L  beta0 beta1\n');
+    for k = 1:maxIters;
+        g = computeGradientNegMLE(y, tX, beta); % gradient
+        L = computeCostNegMLE(y, tX, beta); % cost
+        beta = beta - alpha .* g; % gradient descent to update beta
+
+        if g'*g < 1e-5; % convergence
+           fprintf('CONVERGED: #iter=%i\n', k)
+           break; 
+        end;
+
+        beta_all(:,k) = beta;
+        L_all(k) = L;
+
+        fprintf('%.2f  %.2f %.2f\n', L, beta(1), beta(2));
+
+        % Overlay on the contour plot
+        % For this to work you first have to run grid Search
+        %subplot(121);
+        %plot(beta(1), beta(2), 'o', 'color', 0.7*[1 1 1], 'markersize', 12);
+%         pause(.5) % wait half a second
+% 
+%         % visualize function f on the data
+%         subplot(122);
+%         x = [1.2:.01:2]; % height from 1m to 2m
+%         x_normalized = (x - meanX)./stdX;
+%         f = beta(1) + beta(2).*x_normalized;
+%         plot(height, weight,'.');
+%         hold on;
+%         plot(x,f,'r-');
+%         hx = xlabel('x');
+%         hy = ylabel('y');
+%         hold off;
     end
 end
 
-function g = computeGradient(y,tX,beta)
-    e = logFun(tX*beta) - y;    %compute error
-    g = tX'*e;   
+function [ g ] = computeGradientNegMLE( y, tX, beta )
+% compute gradient corresponding to the log-likelihood
+    g = tX' * (logisticSigma(tX*beta) - y);
+    %g = g ./ length(y);
 end
 
-function H = computeHessian(tx, beta)
-    [N,~] = size(tx);
-    S = zeros(N);
-    for i = 1:N
-        l = logFun(tx(i,:)*beta);
-        S(i,i) = l*(1-l);
+function [ r ] = logisticSigma(x)
+    r = 1 ./ (1 + exp(-x))
+end
+
+function [ L ] = computeCostNegMLE(y, tX, beta)
+% return negative of the value of log-likelihood
+    % this is the log of the likelihood
+
+    L = 0;
+    for n = 1:length(y)
+        L = L + y(n) * (tX(n,:) * beta) - log(1 + exp(tX(n,:) * beta));
     end
-    H = tx'* S * tx;
-end
-
-function o = logFun(x)
-    o2 = 1/(1+exp(-x));
-    o = o2(:,1);
+L = -L;
 end
