1b36d4a10d41d4a03c21354213e221d1d5d8af66
Mickaël Misbach <mickael@misba.ch>
2015-11-03 09:55:05 +0100

    penLogisticRegression implementation ; cleaningInput update

--- penLogisticRegression.m
+++ penLogisticRegression.m
@@ -1,3 +1,6 @@
 function beta = penLogisticRegression(y,tX,alpha,lambda)
-
+    
+    beta = logisticRegression(y, tX, alpha);
+    reg_term = lambda * sum(beta .^2);
+    beta = beta + reg_term;
 end
